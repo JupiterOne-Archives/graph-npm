508732629b22c7f03ba48c71f364a279614aa432
benbrewerbowman <benbrewerbowman@gmail.com>
2020-04-24 14:35:17 -0600

    update to sdk 0.11, added gitleaks

--- test/polly.ts
+++ test/polly.ts
@@ -1,5 +1,3 @@
-import { Har } from 'har-format';
-
 import { Polly } from '@pollyjs/core';
 import NodeHttpAdapter from '@pollyjs/adapter-node-http';
 import FSPersister from '@pollyjs/persister-fs';
@@ -9,46 +7,12 @@ Polly.register(FSPersister);
 
 export { Polly } from '@pollyjs/core';
 
-class TrendMicroFSPersister extends FSPersister {
-  static get id(): string {
-    return 'TrendMicroFSPersister';
-  }
-
-  saveRecording(recordingId: number, data: Har): void {
-    data.log.entries.forEach((entry) => {
-      
-      // Redact tokens, even though they expire
-      entry.request.headers.forEach((header) => {
-        if (header.name === 'authorization') {
-          header.value = '[REDACTED]';
-        }
-      });
-
-      // Redact all cookie values
-      entry.response.headers = entry.response.headers.map((e) => {
-        if (e.name === 'set-cookie') {
-          return { ...e, value: '[REDACTED]' };
-        } else {
-          return e;
-        }
-      });
-
-      entry.response.cookies = entry.response.cookies.map((e) => ({
-        ...e,
-        value: '[REDACTED]',
-      }));
-    });
-
-    super.saveRecording(recordingId, data);
-  }
-}
-
 export function setupPolly(recordingsDir: string, name: string): Polly {
   return new Polly(name, {
     adapters: ['node-http'],
-    persister: TrendMicroFSPersister,
+    persister: FSPersister,
     persisterOptions: {
-      TrendMicroFSPersister: {
+      FSPersister: {
         recordingsDir: `${recordingsDir}/__recordings__`,
       },
     },
